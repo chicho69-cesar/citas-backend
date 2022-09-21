5ac15c9eb54d26ea2b32c5d14278a85385293856
Cesar <cesarvillalobosolmos.01@gmail.com>
2022-09-21 00:35:34 -0500

    New design for database

---
+++ citas-backend/citas-backend/Migrations/20220921052237_AddEntities.cs
@@ -0,0 +1,326 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace citas_backend.Migrations
+{
+    public partial class AddEntities : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Degree",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "varchar(150)", unicode: false, maxLength: 150, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Degree", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Hobbies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "varchar(100)", unicode: false, maxLength: 100, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Hobbies", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "varchar(100)", unicode: false, maxLength: 100, nullable: true),
+                    LastName = table.Column<string>(type: "varchar(150)", unicode: false, maxLength: 150, nullable: true),
+                    Token = table.Column<string>(type: "varchar(max)", unicode: false, nullable: true),
+                    Email = table.Column<string>(type: "varchar(150)", unicode: false, maxLength: 150, nullable: true),
+                    NormalizedEmail = table.Column<string>(type: "varchar(150)", unicode: false, maxLength: 150, nullable: true),
+                    UserName = table.Column<string>(type: "varchar(100)", unicode: false, maxLength: 100, nullable: true),
+                    NormalizedUserName = table.Column<string>(type: "varchar(100)", unicode: false, maxLength: 100, nullable: true),
+                    Password = table.Column<string>(type: "varchar(max)", unicode: false, nullable: true),
+                    BirthDate = table.Column<DateTime>(type: "date", nullable: true),
+                    Description = table.Column<string>(type: "varchar(550)", unicode: false, maxLength: 550, nullable: true),
+                    Genre = table.Column<string>(type: "varchar(20)", unicode: false, maxLength: 20, nullable: true),
+                    ImageProfile = table.Column<string>(type: "varchar(500)", unicode: false, maxLength: 500, nullable: true),
+                    IdDegree = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK__Users__IdDegree__267ABA7A",
+                        column: x => x.IdDegree,
+                        principalTable: "Degree",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Dates",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Date = table.Column<DateTime>(type: "datetime", nullable: false),
+                    Place = table.Column<string>(type: "varchar(1000)", unicode: false, maxLength: 1000, nullable: true),
+                    Description = table.Column<string>(type: "varchar(550)", unicode: false, maxLength: 550, nullable: true),
+                    Grade = table.Column<double>(type: "float", nullable: true),
+                    IdUserFirst = table.Column<int>(type: "int", nullable: false),
+                    IdUserSecond = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Dates", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK__Dates__IdUserFir__3F466844",
+                        column: x => x.IdUserFirst,
+                        principalTable: "Users",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK__Dates__IdUserSec__403A8C7D",
+                        column: x => x.IdUserSecond,
+                        principalTable: "Users",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Messages",
+                columns: table => new
+                {
+                    IdMessage = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Message = table.Column<string>(type: "varchar(1200)", unicode: false, maxLength: 1200, nullable: true),
+                    Date = table.Column<DateTime>(type: "datetime", nullable: false),
+                    IdUserSend = table.Column<int>(type: "int", nullable: false),
+                    IdUserRecieve = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Messages", x => x.IdMessage);
+                    table.ForeignKey(
+                        name: "FK__Messages__IdUser__3B75D760",
+                        column: x => x.IdUserSend,
+                        principalTable: "Users",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK__Messages__IdUser__3C69FB99",
+                        column: x => x.IdUserRecieve,
+                        principalTable: "Users",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Description = table.Column<string>(type: "varchar(550)", unicode: false, maxLength: 550, nullable: true),
+                    Date = table.Column<DateTime>(type: "datetime", nullable: false),
+                    Likes = table.Column<int>(type: "int", nullable: false),
+                    IdUser = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK__Posts__IdUser__29572725",
+                        column: x => x.IdUser,
+                        principalTable: "Users",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SocialNetworks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: true),
+                    Link = table.Column<string>(type: "varchar(1000)", unicode: false, maxLength: 1000, nullable: true),
+                    IdUser = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SocialNetworks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK__SocialNet__IdUse__38996AB5",
+                        column: x => x.IdUser,
+                        principalTable: "Users",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserHobbies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IdUser = table.Column<int>(type: "int", nullable: false),
+                    IdHobbie = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserHobbies", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK__UserHobbi__IdHob__32E0915F",
+                        column: x => x.IdHobbie,
+                        principalTable: "Hobbies",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK__UserHobbi__IdUse__31EC6D26",
+                        column: x => x.IdUser,
+                        principalTable: "Users",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Comments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Comment = table.Column<string>(type: "varchar(550)", unicode: false, maxLength: 550, nullable: true),
+                    Date = table.Column<DateTime>(type: "datetime", nullable: false),
+                    IdUser = table.Column<int>(type: "int", nullable: false),
+                    IdPost = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK__Comments__IdPost__2D27B809",
+                        column: x => x.IdPost,
+                        principalTable: "Posts",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK__Comments__IdUser__2C3393D0",
+                        column: x => x.IdUser,
+                        principalTable: "Users",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PostImages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ImageUrl = table.Column<string>(type: "varchar(500)", unicode: false, maxLength: 500, nullable: true),
+                    IdPost = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PostImages", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK__PostImage__IdPos__35BCFE0A",
+                        column: x => x.IdPost,
+                        principalTable: "Posts",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_IdPost",
+                table: "Comments",
+                column: "IdPost");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_IdUser",
+                table: "Comments",
+                column: "IdUser");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Dates_IdUserFirst",
+                table: "Dates",
+                column: "IdUserFirst");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Dates_IdUserSecond",
+                table: "Dates",
+                column: "IdUserSecond");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_IdUserRecieve",
+                table: "Messages",
+                column: "IdUserRecieve");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_IdUserSend",
+                table: "Messages",
+                column: "IdUserSend");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PostImages_IdPost",
+                table: "PostImages",
+                column: "IdPost");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Posts_IdUser",
+                table: "Posts",
+                column: "IdUser");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SocialNetworks_IdUser",
+                table: "SocialNetworks",
+                column: "IdUser");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserHobbies_IdHobbie",
+                table: "UserHobbies",
+                column: "IdHobbie");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserHobbies_IdUser",
+                table: "UserHobbies",
+                column: "IdUser");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_IdDegree",
+                table: "Users",
+                column: "IdDegree");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Comments");
+
+            migrationBuilder.DropTable(
+                name: "Dates");
+
+            migrationBuilder.DropTable(
+                name: "Messages");
+
+            migrationBuilder.DropTable(
+                name: "PostImages");
+
+            migrationBuilder.DropTable(
+                name: "SocialNetworks");
+
+            migrationBuilder.DropTable(
+                name: "UserHobbies");
+
+            migrationBuilder.DropTable(
+                name: "Posts");
+
+            migrationBuilder.DropTable(
+                name: "Hobbies");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Degree");
+        }
+    }
+}
